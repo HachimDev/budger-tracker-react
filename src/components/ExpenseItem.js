c8599b4f4323f406b4d15d6f5a5475622ac6bece
Hachim JABRI <hachim.jabri@gmail.com>
2022-04-04 17:30:15 +0200

    add context

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { TiDelete } from "react-icons/ti";
+
+const ExpenseItem = ({ id, name, cost }) => {
+  return (
+    <li className="list-group-item d-flex justify-content-between align-items-center">
+      {name}
+      <div>
+        <span className="badge rounded-pill bg-primary me-3">{cost}</span>
+        <TiDelete size="1.5em"></TiDelete>
+      </div>
+    </li>
+  );
+};
+
+export default ExpenseItem;
